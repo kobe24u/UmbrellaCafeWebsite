04465e79006e1c079d4badd39b3894028daa9632
Priyanka <priyankag2412@gmail.com>
2017-06-28 13:36:22 +1000

    Created links to views; controllers; l

---
+++ UmbrellaCafeWebsite/Controllers/CafesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmbrellaCafeWebsite.Controllers
+{
+    public class CafesController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Cafes";
+            return View ();
+        }
+    }
+}
