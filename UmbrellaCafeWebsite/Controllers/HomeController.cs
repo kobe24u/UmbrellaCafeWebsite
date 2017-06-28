a927677489c18d37be77a1bde5654485a7c62b80
Priyanka <priyankag2412@gmail.com>
2017-06-28 18:11:23 +1000

    Latest version.

--- UmbrellaCafeWebsite/Controllers/HomeController.cs
+++ UmbrellaCafeWebsite/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +21,11 @@ namespace UmbrellaCafeWebsite.Controllers
             return View();
         }
 
+		public ActionResult About()
+		{
+			
+			return View();
+		}
 
     }
 }
